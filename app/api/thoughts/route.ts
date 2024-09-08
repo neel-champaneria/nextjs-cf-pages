9d6709ae1116b6223c4a6273f93921ad52578670
Neel Champaneria <neel@promactinfo.com>
2024-09-08 16:15:55 +0530

    first commit

---
+++ app/api/thoughts/route.ts
@@ -0,0 +1,21 @@
+import db from "@/libs/db";
+
+import { NextRequest, NextResponse } from "next/server";
+
+// export const runtime =
+//   process.env.NODE_ENV === "development" ? "nodejs" : "edge";
+
+export const runtime = "edge";
+
+export async function POST(request: NextRequest) {
+  const { text } = await request.json();
+  const thought = await db.thought.create({
+    data: { text },
+  });
+  return Response.json({ message: "Thought Created" }, { status: 201 });
+}
+
+export async function GET(request: NextRequest) {
+  const thoughts = await db.thought.findMany();
+  return Response.json({ thoughts });
+}
---
+++ libs/db.ts
@@ -0,0 +1,24 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const db = new PrismaClient();
+
+// export default db;
+
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+// import dotenv from "dotenv";
+// import ws from "ws";
+
+// dotenv.config();
+// neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.POSTGRES_PRISMA_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+let db: PrismaClient =
+  process.env.NODE_ENV === "development"
+    ? new PrismaClient()
+    : new PrismaClient({ adapter });
+
+export default db;
